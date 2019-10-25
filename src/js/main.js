3576bb7fc3f6acb6931f5713d69132b65f44a3be
Taylor Hemphill <54860461+THemphill23@users.noreply.github.com>
2019-10-25 15:11:49 -0400

    User Action

--- src/js/main.js
+++ src/js/main.js
@@ -2,27 +2,24 @@ import blackjack from "blackjack-dealer-logic";
 
 
 export default () =>{
-  alert("Welcome");
   const game =  blackjack.singleDeckGame;
   const gameIsRunning = true;
   const playButton = document.getElementById("playButton");
-  const betButton = document.getElementById("betButton")
+  const hitButton = document.getElementById("hitButton")
 
   playButton.onclick = function(){
     // while (gameIsRunning){
       // alert("Let' Play BlackJack!" + game.getUserChips())
 
-      document.getElementById("board")
-      board.innerHTML = `Your current chip count is: ${game.getUserChips()}`
+      document.getElementById("chips")
+      chips.innerHTML = `Your current chip count is: ${game.getUserChips()}`
 
-      // betButton.onclick = function(){
-        const wager =window.prompt("Enter your bet:")
+      const wager = window.prompt("Enter your bet:")
         
-        game.receiveAnte(wager);
+      game.receiveAnte(wager);
         
-      document.getElementById("field")
-      field.innerHTML = `Your bet is: ${wager}`
-      // }
+      document.getElementById("bet")
+      bet.innerHTML = `Your bet is: ${wager}`
       game.deal();
       
       document.getElementById("dealer")
@@ -31,8 +28,30 @@ export default () =>{
       document.getElementById("player")
       player.innerHTML = `Your current hand: ${game.getUserHandValue()}`
 
+      document.getElementById("board")
+      board.innerHTML = `What would you like to do?`
 
-      game.resetPlayers();
+      const userAction = window.prompt("1:HIT   2:STAND   3:DOUBLE")
+        switch(userAction){
+          case "1":
+          break;
+
+          case "2":
+            break;
+
+
+          case "3":
+            break;
+
+          default:
+            break;
+        }
+        
+
+      // game.resetPlayers();
     }
-
+    
+    // hitButton.onclick = fucntion(){
+    //     game.hitUser()
+    // }
   }
