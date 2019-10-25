ecbaed79528204a80cda115dddfc79af6fddef49
Taylor Hemphill <54860461+THemphill23@users.noreply.github.com>
2019-10-25 14:44:01 -0400

    Deal Displays

--- src/js/main.js
+++ src/js/main.js
@@ -2,25 +2,37 @@ import blackjack from "blackjack-dealer-logic";
 
 
 export default () =>{
-  alert("It works!");
+  alert("Welcome");
   const game =  blackjack.singleDeckGame;
   const gameIsRunning = true;
   const playButton = document.getElementById("playButton");
-
+  const betButton = document.getElementById("betButton")
 
   playButton.onclick = function(){
     // while (gameIsRunning){
-      alert("Let' Play BlackJack!" + game.getUserChips())
+      // alert("Let' Play BlackJack!" + game.getUserChips())
+
       document.getElementById("board")
       board.innerHTML = `Your current chip count is: ${game.getUserChips()}`
-      // game.wager()
-      // game.receiveAnte(wager);
-      // game.getAnte()
-      // document.write();
+
+      // betButton.onclick = function(){
+        const wager =window.prompt("Enter your bet:")
+        
+        game.receiveAnte(wager);
+        
+      document.getElementById("field")
+      field.innerHTML = `Your bet is: ${wager}`
+      // }
+      game.deal();
+      
+      document.getElementById("dealer")
+      dealer.innerHTML = `Dealer is showing: ${game.getDealerCardUp()}`
+
+      document.getElementById("player")
+      player.innerHTML = `Your current hand: ${game.getUserHandValue()}`
+
 
       game.resetPlayers();
     }
 
   }
-  
-  
