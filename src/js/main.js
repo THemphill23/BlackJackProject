242f7847473d81e4f37fe8b29db1ac040d1bb07f
Taylor Hemphill <54860461+THemphill23@users.noreply.github.com>
2019-10-25 13:49:26 -0400

    staging

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 import blackjack from "blackjack-dealer-logic";
+import { question } from "readline-sync";
 
 
 
@@ -7,32 +8,21 @@ export default () =>{
   const game =  blackjack.singleDeckGame;
   const gameIsRunning = true;
   const playButton = document.getElementById("playButton");
-  // const hitButton = document.getElementById("hitButton");
-  // const standButton = document.getElementById("standButton");
-  // const doubleButton = document.getElementById("doubleButton");
 
 
   playButton.onclick = function(){
     // while (gameIsRunning){
-      // alert("Let' Play BlackJack!" + game.getUserChips())
+      alert("Let' Play BlackJack!" + game.getUserChips())
       document.getElementById("board")
       board.innerHTML = `Your current chip count is: ${game.getUserChips()}`
-      const wager = Number(ask("Enter you bet"));
+      game.wager()
       game.receiveAnte(wager);
-      document.write();
+      game.getAnte()
+      // document.write();
 
-      // printCurrentAnte();
-      // game.deal();
-    
       game.resetPlayers();
     }
 
   }
-
-  // function printCurrentAnte () {
-  //   document.write(`Current Ante: ${game.getAnte()}`);
-  // }
-// }
-
   
   
