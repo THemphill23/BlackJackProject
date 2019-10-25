c171ec5287272aadea8ba108542b48eb5e039a6a
Taylor Hemphill <54860461+THemphill23@users.noreply.github.com>
2019-10-25 10:26:42 -0400

    Loop Working

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,40 @@
-export default () => {
-    alert("it works!")
+import blackjack from "blackjack-dealer-logic";
+
+
+
+export default () =>{
+  alert("It works!");
+  const game =  blackjack.singleDeckGame;
+  const gameIsRunning = true;
+  const playButton = document.getElementById("playButton");
+  const hitButton = document.getElementById("hitButton");
+  const standButton = document.getElementById("standButton");
+  const doubleButton = document.getElementById("doubleButton");
+
+
+  playButton.onclick = function(){
+    while (gameIsRunning){
+      alert("Let' Play BlackJack!" + game.getUserChips())
+      game.deal();
+      
+    
+      game.resetPlayers();
+    }
+
   }
+
+  
+
+  
+  // const deal = function (){
+   
+  // }
+  // const app = document.getElementById("app");
+
+  // playButton.onclick = function() {
+  // alert("Let's Play BlackJack!")
+  // }
+}
+
+  
+  
